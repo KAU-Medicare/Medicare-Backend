038e20ce8c140977b1e815a1deba1ca18f001280
You-jisang <dbwltkd1019@naver.com>
2024-11-30 18:09:24 +0900

    refactoring: 클린 코드 & 리펙토링

--- src/main/java/com/example/kaumedicare/AllergyInference/model/AnalysisReason.java
+++ src/main/java/com/example/kaumedicare/AllergyInference/model/AnalysisReason.java
@@ -6,10 +6,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Table(name = "analysis_reasons")
 @Getter
